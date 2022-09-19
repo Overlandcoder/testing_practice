1261cbe9d46dd019a9fcac56b518de39fa025b28
Overlander <anuragnandal@hotmail.com>
2022-09-19 09:25:26 -0400

    Add 3 basic tests using FactoryBot and Rails model

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,2 +1,11 @@
 class PhoneNumber < ApplicationRecord
+  EXPECTED_NUMBER_OF_DIGITS = 10
+  before_validation :strip_non_numeric_from_value
+
+  def strip_non_numeric_from_value
+    self.value = self.value.gsub(/\D/, "")
+    .split("")
+    .last(EXPECTED_NUMBER_OF_DIGITS)
+    .join
+  end
 end
--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "phone number contains dashes" do
+    it "strips out the dashes" do
+      phone_number = FactoryBot.create(
+        :phone_number,
+        value: "555-856-8075"
+      )
+
+      expect(phone_number.value).to eq("5558568075")
+    end
+  end
+
+  context "phone number contains parentheses" do
+    it "strips out the non-numeric characters" do
+      phone_number = FactoryBot.create(
+        :phone_number,
+        value: "(555) 856-8075"
+      )
+
+      expect(phone_number.value).to eq("5558568075")
+    end
+  end
+
+  context "phone number contains country code" do
+    it "strips out the country code" do
+      phone_number = FactoryBot.create(
+        :phone_number,
+        value: "+1 555 856 8075"
+      )
+
+      expect(phone_number.value).to eq("5558568075")
+    end
+  end
 end
