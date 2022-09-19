f0500ed48f738147643aa0183cd1c0551f4f05a6
Overlander <anuragnandal@hotmail.com>
2022-09-19 09:09:53 -0400

    Add 3 simple tests

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,2 +1,11 @@
-class PhoneNumber < ApplicationRecord
+class PhoneNumber
+  attr_reader :value
+  EXPECTED_NUMBER_OF_DIGITS = 10
+
+  def initialize(value)
+    @value = value.gsub(/\D/, "")
+    .split("")
+    .last(EXPECTED_NUMBER_OF_DIGITS)
+    .join
+  end
 end
---
+++ spec/models/phone_number_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe PhoneNumber, type: :model do
+  context 'phone number contains dashes' do
+    it 'strips out the dashes' do
+      phone_number = PhoneNumber.new("555-856-8075")
+
+      expect(phone_number.value).to eq("5558568075")
+    end
+  end
+
+  context 'phone number contains parentheses' do
+    it 'strips out the non-numeric characters' do
+      phone_number = PhoneNumber.new("(555) 856-8075")
+
+      expect(phone_number.value).to eq("5558568075")
+    end
+  end
+
+  context 'phone number contains country code' do
+    it 'strips out the country code' do
+      phone_number = PhoneNumber.new("+1 555 856 8075")
+
+      expect(phone_number.value).to eq("5558568075")
+    end
+  end
+end
