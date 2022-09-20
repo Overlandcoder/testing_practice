70b0bb2fef6bfb14781eb9ffa0f0610016e36d11
Overlander <anuragnandal@hotmail.com>
2022-09-20 13:56:02 -0400

    Change root route and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_19_131123) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_175334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "phone_numbers", force: :cascade do |t|
     t.string "value"
